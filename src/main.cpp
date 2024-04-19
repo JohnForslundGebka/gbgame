6bee5210147e272089909c7f5619304b621ac375
John Forslund Gebka <johnforslundsmail@gmail.com>
2024-04-19 11:27:06 +0200

    add boiler plate code to test connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,28 @@
 #include <Arduino.h>
+#include "mbed.h"
+#include "rtos.h"
+
+
+// Create a DigitalOut object to control the LED
+mbed::DigitalOut led(LED1);
+
+// Function that toggles the LED in a thread
+void toggleLED() {
+
+    while (true) {
+        led = !led;  // Toggle the state of the LED
+        rtos::ThisThread::sleep_for(500);  // Sleep for 500 milliseconds
+    }
+}
+
+// Main thread object
+rtos::Thread thread;
+
 void setup() {
-// write your initialization code here
+    // Start the thread, running the toggleLED function
+    thread.start(mbed::callback(toggleLED));
 }
 
 void loop() {
-// write your code here
+
 }
