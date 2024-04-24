56b3bc7940bd9e75eac81cd962cd583f7eaafbd6
John Forslund Gebka <johnforslundsmail@gmail.com>
2024-04-24 11:36:48 +0200

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,26 @@
 #include <Arduino.h>
 #include "mbed.h"
 #include "rtos.h"
-#include "core/arduinoPinNames.h"
-
-using namespace mbed;
-using namespace rtos;
-
-InterruptIn button(pin::BTN_A,PullUp);
-InterruptIn buttonB(pin::BTN_B,PullUp);
-Thread thread1;
-EventFlags flags;
-
-#define FLAG1 (1UL << 1)
-#define FLAG2 (1UL << 2)
-#define FLAG3 (1UL << 3)
-#define FLAG4 (1UL << 4)
-
-void print(){
 
 
+// Create a DigitalOut object to control the LED
+mbed::DigitalOut led(LED1);
 
-    while(flags.wait_any(FLAG1 | FLAG2 | FLAG3 | FLAG4, osWaitForever,false)) {
-       switch (flags.get())
-       {
-           case FLAG1 :
-               Serial.println("SAK 1");
-               flags.clear();
-               break;
-           case FLAG2:
-               Serial.println("SAK 2");
-               flags.clear();
-               break;
-           case FLAG3:
-               Serial.println("SAK 3");
-               flags.clear();
-               break;
-           case FLAG4:
-               Serial.println("SAK 4");
-               flags.clear();
-               break;
-           default:
-               break;
-       }
-      //  ThisThread::sleep_for(std::chrono::milliseconds(500));
+// Function that toggles the LED in a thread
+void toggleLED() {
+
+    while (true) {
+        led = !led;  // Toggle the state of the LED
+        rtos::ThisThread::sleep_for(500);  // Sleep for 500 milliseconds
     }
 }
 
-void on_button_press() {
-   flags.set(FLAG1);
-}
-
-void on_button_press2(){
-    flags.set(FLAG2);
-}
+// Main thread object
+rtos::Thread thread;
 
 void setup() {
-    Serial.begin(9600);
-    button.fall(&on_button_press);
-    buttonB.fall(&on_button_press2);
-    thread1.start(print);
+    // Start the thread, running the toggleLED function
+    thread.start(mbed::callback(toggleLED));
 }
 
 void loop() {
