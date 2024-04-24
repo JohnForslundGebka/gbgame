39df8287153013d98d0a2bcfeaecfe8f466ab11a
John Forslund Gebka <johnforslundsmail@gmail.com>
2024-04-24 16:22:59 +0200

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,12 @@
 #include "rtos.h"
 #include "hardware/buttons.h"
 
-
 #define DC_PIN   15   // Data/Command
 #define CS_PIN   14   // Chip Select, can use any GPIO except A6/A7
 #define RST_PIN  9    // Reset
 
 // Create an instance of the display
-Adafruit_SSD1351 display = Adafruit_SSD1351(128, 128, &SPI1, CS_PIN, DC_PIN, RST_PIN);
+Adafruit_SSD1351 display = Adafruit_SSD1351(128, 128, &::SPI, CS_PIN, DC_PIN, RST_PIN);
 GFXcanvas16 canvas(128, 128);
 
 using namespace mbed;
@@ -28,7 +27,7 @@ int ballX = 10;
 int ballY = 10;
 
 void setup() {
-    SPI1.begin();
+    ::SPI.begin();
     Serial.begin(9600);
     display.begin(13000000);
     display.fillScreen(0x0000);
