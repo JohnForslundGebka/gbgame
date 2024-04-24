8e9bf1594949ecb07f61d5b0b3acd94c2e1a5fbd
John Forslund Gebka <johnforslundsmail@gmail.com>
2024-04-24 16:16:56 +0200

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,19 @@
+#include <Arduino.h>
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1351.h>
+#include <Serial.h>
 #include "mbed.h"
 #include "rtos.h"
 #include "hardware/buttons.h"
-#include <Arduino.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1351.h>
-#include <SPI.h>
+
 
 #define DC_PIN   15   // Data/Command
 #define CS_PIN   14   // Chip Select, can use any GPIO except A6/A7
 #define RST_PIN  9    // Reset
 
 // Create an instance of the display
-Adafruit_SSD1351 display = Adafruit_SSD1351(128, 128, &SPI, CS_PIN, DC_PIN, RST_PIN);
+Adafruit_SSD1351 display = Adafruit_SSD1351(128, 128, &SPI1, CS_PIN, DC_PIN, RST_PIN);
 GFXcanvas16 canvas(128, 128);
 
 using namespace mbed;
@@ -26,7 +28,7 @@ int ballX = 10;
 int ballY = 10;
 
 void setup() {
-    SPI.begin();
+    SPI1.begin();
     Serial.begin(9600);
     display.begin(13000000);
     display.fillScreen(0x0000);
@@ -38,6 +40,9 @@ void loop() {
 
     // Draw the ball at the new position
     canvas.fillCircle(ballX, ballY, 3, 0xA800);
+
+    //draw canvas
+    display.drawRGBBitmap(0, 0, canvas.getBuffer(), canvas.width(), canvas.height());
 
     flagsRead = buttons.buttonStates.wait_any(
             Buttons::UP_FLAG |
