f3673803f652351de7802b102c1440a7c027be63
cyberhelmer <petterson.melker@gmail.com>
2024-04-25 15:45:09 +0200

    Changed main

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,28 @@
 #include <Arduino.h>
-#include <SPI.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1351.h>
-#include <Serial.h>
 #include "mbed.h"
 #include "rtos.h"
-#include "hardware/buttons.h"
 
-using namespace mbed;
-using namespace rtos;
 
-//Buttons object
-Buttons buttons;
+// Create a DigitalOut object to control the LED
+mbed::DigitalOut led(LED1);
 
+// Function that toggles the LED in a thread
+void toggleLED() {
+
+    while (true) {
+        led = !led;  // Toggle the state of the LED
+        rtos::ThisThread::sleep_for(500);  // Sleep for 500 milliseconds
+    }
+}
+
+// Main thread object
+rtos::Thread thread;
 
 void setup() {
-    Serial.begin(9600);
+    // Start the thread, running the toggleLED function
+    thread.start(mbed::callback(toggleLED));
 }
 
 void loop() {
-    //Wait for any of ALL_flags to be set.
-    switch (buttons.states.wait_any(Buttons::ALL_FLAG)) {
-        case Buttons::UP_FLAG:
-            Serial.println("Button UP");
-            break;
-        case Buttons::LEFT_FLAG:
-            Serial.println("Button LEFT");
-            break;
-        case Buttons::RIGHT_FLAG:
-            Serial.println("Button RIGHT");
-            break;
-        case Buttons::DOWN_FLAG:
-            Serial.println("Button DOWN");
-            break;
-        case Buttons::A_FLAG:
-            Serial.println("Button A");
-            break;
-        case Buttons::B_FLAG:
-            Serial.println("Button B");
-            break;
-        case Buttons::START_FLAG:
-            Serial.println("Button START");
-            break;
-        default:
-            break;
-    }
 
-    
 }
