989fbb2a61597b53c378357fca7bf7078ace144b
John Forslund Gebka <johnforslundsmail@gmail.com>
2024-04-26 11:37:31 +0200

    add macro.h file for defines

--- src/main.cpp
+++ src/main.cpp
@@ -8,22 +8,8 @@
 #include "hardware/buttons.h"
 #include "core/state.h"
 #include "game/stateHandler.h"
-
-#define DC_PIN   15   // Data/Command
-#define CS_PIN   14   // Chip Select, can use any GPIO except A6/A7
-#define RST_PIN  9    // Reset
-
-// Color definitions
-#define BLACK 0x0000
-#define BLUE 0x001F
-#define RED 0xF800
-#define GREEN 0x07E0
-#define CYAN 0x07FF
-#define MAGENTA 0xF81F
-#define YELLOW 0xFFE0
-#define WHITE 0xFFFF
-
-#define MOV_FLAG  (1UL << 2)
+#include "macros.h"
+#include "core/macros.h"
 
 // Create an instance of the display
 Adafruit_SSD1351 display = Adafruit_SSD1351(128, 128, &::SPI, CS_PIN, DC_PIN, RST_PIN);
