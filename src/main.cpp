55664b1cb6a85b5e90980d3be1eeeb7940e03ef2
John Forslund Gebka <johnforslundsmail@gmail.com>
2024-04-26 14:26:27 +0200

    add namespaces

--- src/main.cpp
+++ src/main.cpp
@@ -8,22 +8,20 @@ using namespace std::chrono;
 
 // Create a DigitalOut object to control the LED
 DigitalOut led(LED1);
+// Main thread object
+Thread thread;
 
 // Function that toggles the LED in a thread
 void toggleLED() {
-
     while (true) {
         led = !led;  // Toggle the state of the LED
         ThisThread::sleep_for(milliseconds(500));
     }
 }
 
-// Main thread object
-rtos::Thread thread;
-
 void setup() {
     // Start the thread, running the toggleLED function
-    thread.start(mbed::callback(toggleLED));
+    thread.start(callback(toggleLED));
 }
 
 void loop() {
