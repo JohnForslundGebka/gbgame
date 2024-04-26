7d1b64f8e74dcfb4fa84d7e7cb82d26a76b4818b
John Forslund Gebka <johnforslundsmail@gmail.com>
2024-04-26 14:18:37 +0200

    add comments

--- src/game/stateHandler.cpp
+++ src/game/stateHandler.cpp
@@ -1,5 +1,9 @@
 #include "stateHandler.h"
-
+/**
+ * @brief Handle inputs for the current state.
+ *
+ * This method delegates the input handling to the active state.
+ */
 void StateHandler::handleInput() {
     m_currentState->handleInput();
 }
@@ -7,12 +11,21 @@ void StateHandler::handleInput() {
 void StateHandler::updateState() {
     m_currentState->update();
 }
-
+/**
+     * @brief Run the active state.
+     *
+     *
+     *
+     */
 void StateHandler::run(){
     m_currentState->stop();
     m_currentState->run();
 }
-
+/**
+    * @brief Set the current state of the game or application.
+    *
+    * @param currentState The new state to be managed.
+    */
 void StateHandler::setState(State &currentState) {
     m_currentState->stop();
     m_currentState = &currentState;
--- src/game/stateHandler.h
+++ src/game/stateHandler.h
@@ -1,3 +1,17 @@
+//stateHandler.h
+/**
+ * @brief This is a class that is responsible for running and stopping the different states of the machine
+ *
+ *
+ *   The StateHandler class is designed to control the transition between different states
+ *   of a game or application, such as menus, gameplay, pause screens, etc. It ensures that
+ *   the current state is correctly managed and that state transitions are handled smoothly.
+ *
+ *
+ *
+ *
+ *
+ */
 #ifndef GBGAME_STATEHANDLER_H
 #define GBGAME_STATEHANDLER_H
 #include "core/state.h"
