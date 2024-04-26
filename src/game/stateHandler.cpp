7e0b93caf65eb6c9bc5647fff70cc30d1d380409
John Forslund Gebka <johnforslundsmail@gmail.com>
2024-04-26 11:18:17 +0200

    add member functions and variables

--- src/game/stateHandler.cpp
+++ src/game/stateHandler.cpp
@@ -1,5 +1,1 @@
-//
-// Created by John Jakobsson forslund on 2024-04-26.
-//
-
 #include "stateHandler.h"
--- src/game/stateHandler.h
+++ src/game/stateHandler.h
@@ -2,6 +2,18 @@
 #define GBGAME_STATEHANDLER_H
 
 class StateHandler {
+public:
+    //constructor
+    explicit StateHandler(State* currentState) : m_currentState(currentState) {}
+
+    void setState();
+    void handleInput();
+    void updateState();
+    void opperator();
+
+private:
+    //a pointer to the current state that is running at the moment
+    State* m_currentState;
 
 };
 
