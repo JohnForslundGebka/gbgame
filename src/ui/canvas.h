3cce499fc41ae119df8f2f7c650edc8d94f54039
John Forslund Gebka <johnforslundsmail@gmail.com>
2024-04-30 14:14:16 +0200

    add comments

--- src/ui/canvas.h
+++ src/ui/canvas.h
@@ -4,31 +4,28 @@
 
 class Canvas {
 public:
+    //the GFXcanvas16 object that is used to draw things to the display
     GFXcanvas16 C;
+    //getter and setter functions
     int16_t getX() const;
-
     void setX(int16_t x);
-
     int16_t getY() const;
-
     void setY(int16_t y);
 
     int16_t getOldX() const;
-
     void setOldX(int16_t oldX);
-
     int16_t getOldY() const;
-
     void setOldY(int16_t oldY);
-
+  //update the x and y position
     void updatePos();
 
     //constructor
     Canvas(uint16_t width, uint16_t height, int16_t x, int16_t y);
 private:
-    int16_t x = 0;
-    int16_t y = 0;
-    int16_t oldX = 0;
-    int16_t oldY = 0;
+    //member variables that holds the current position and the previous position
+    int16_t m_x = 0;
+    int16_t m_y = 0;
+    int16_t m_oldX = 0;
+    int16_t m_oldY = 0;
 };
 #endif //GBGAME_CANVAS_H
