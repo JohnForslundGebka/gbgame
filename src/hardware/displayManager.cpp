0716813558f28ed2bd0ac6a0ea6d40cd89c08051
John Forslund Gebka <johnforslundsmail@gmail.com>
2024-04-30 14:03:17 +0200

    add member variables and functions

--- src/hardware/displayManager.cpp
+++ src/hardware/displayManager.cpp
@@ -1,1 +1,25 @@
 #include "displayManager.h"
+
+// Private constructor definition
+DisplayManager::DisplayManager() : m_canvas(128,128) {
+}
+
+DisplayManager& DisplayManager::getInstance() {
+    static DisplayManager instance;
+    return instance;
+}
+
+void DisplayManager::init() {
+    ::SPI.begin();
+    m_display.begin(17000000); // Initialize display with a specific clock rate
+    m_display.fillScreen(BLACK);
+}
+
+void DisplayManager::updateScreen(Canvas *canvas) {
+    m_displayMutex.lock();
+    m_canvas.fillRect(canvas->getOldX(),canvas->getOldY(),canvas->C.width(),canvas->C.height(),BLACK);
+    m_canvas.drawRGBBitmap(canvas->getX(), canvas->getY(), canvas->C.getBuffer(), canvas->C.width(), canvas->C.height());
+
+    m_display.drawRGBBitmap(0, 0, m_canvas.getBuffer(), m_canvas.width(), m_canvas.height());
+    m_displayMutex.unlock();
+}
--- src/hardware/displayManager.h
+++ src/hardware/displayManager.h
@@ -3,13 +3,30 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1351.h>
 #include <SPI.h>
+#include "rtos.h"
 #include "core/macros.h"
 
-
-
+using namespace rtos;
 class DisplayManager {
 
+public:
+    static DisplayManager& getInstance();
+    // Initialize the display
+    void init();
+    // Update the entire screen based on the provided GFXcanvas16
+    void updateScreen(Canvas *canvas);
+
+    DisplayManager(const DisplayManager&) = delete;
+    DisplayManager& operator=(const DisplayManager&) = delete;
+
+private:
+    Adafruit_SSD1351 m_display = Adafruit_SSD1351(m_ScreenWidth, m_ScreenHeight, &::SPI, CS_PIN, DC_PIN, RST_PIN);
+    Mutex m_displayMutex;
+
+    DisplayManager();
+
+    static constexpr uint16_t m_ScreenWidth = 128;
+    static constexpr uint16_t m_ScreenHeight = 128;
+    GFXcanvas16 m_canvas;
 };
-
-
 #endif //GBGAME_DISPLAYMANAGER_H
