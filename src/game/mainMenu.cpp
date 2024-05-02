b1fa83d0f25fbd748cc71b0efcf90dc31748f021
John Forslund Gebka <johnforslundsmail@gmail.com>
2024-05-02 11:20:52 +0200

    create mainMenu class

--- src/game/mainMenu.cpp
+++ src/game/mainMenu.cpp
@@ -1,7 +1,9 @@
 #include "mainMenu.h"
 
 void MainMenu::handleInput() {
+    while (m_isRunning && Buttons::states.wait_any(Buttons::UP_FLAG | Buttons::DOWN_FLAG, osWaitForever, false)) {
 
+    }
 }
 
 void MainMenu::update() {
@@ -16,4 +18,4 @@ void MainMenu::stop() {
 
 }
 
-MainMenu::MainMenu() : m_canvas(128,128) {}
+MainMenu::MainMenu() : m_textCanvas(128,128,0,0), m_handCanvas(16,11,7,30) {}
