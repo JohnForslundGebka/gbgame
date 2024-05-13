c972a5c72084d3c258f4b161c13d071f270edd19
John Forslund Gebka <johnforslundsmail@gmail.com>
2024-05-13 15:26:17 +0200

    create settings state

---
+++ src/states/settings/settings.h
@@ -0,0 +1,10 @@
+#ifndef GBGAME_SETTINGS_H
+#define GBGAME_SETTINGS_H
+
+
+class settings {
+
+};
+
+
+#endif //GBGAME_SETTINGS_H
---
+++ src/states/settings/settingsUi.h
@@ -0,0 +1,9 @@
+#ifndef GBGAME_SETTINGSUI_H
+#define GBGAME_SETTINGSUI_H
+
+class settingsUi {
+
+};
+
+
+#endif //GBGAME_SETTINGSUI_H
