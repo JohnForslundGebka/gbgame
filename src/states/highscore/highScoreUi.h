04462935383cdc2132f88e7262710dca96f2dbf4
John Forslund Gebka <johnforslundsmail@gmail.com>
2024-05-21 11:47:48 +0200

    add highscoreUi

---
+++ src/states/highscore/highScoreUi.h
@@ -0,0 +1,15 @@
+#ifndef GBGAME_HIGHSCOREUI_H
+#define GBGAME_HIGHSCOREUI_H
+#include "ui/canvas.h"
+#include "ui/images.h"
+#include "core/state.h"
+#include "functionality/scores.h"
+
+class HighScore; // Forward declaration
+
+class HighScoreUi {
+
+};
+
+
+#endif //GBGAME_HIGHSCOREUI_H
