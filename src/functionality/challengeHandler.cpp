408b7b52788128c5163c5b4413e976f86fc60d61
John Forslund Gebka <johnforslundsmail@gmail.com>
2024-05-28 15:33:59 +0200

    add startChallenge function

--- src/functionality/challengeHandler.cpp
+++ src/functionality/challengeHandler.cpp
@@ -1,4 +1,8 @@
 #include "challengeHandler.h"
+#include "challenge.h"
+#include "core/state.h"
+#include "core/macros.h"
+
 
 ChallengeHandler &ChallengeHandler::getInstance() {
     static ChallengeHandler instance;
@@ -6,5 +10,21 @@ ChallengeHandler &ChallengeHandler::getInstance() {
 }
 
 ChallengeHandler::ChallengeHandler() {
+
+}
+
+void ChallengeHandler::startChallenge(Challenge *c) {
+  currentChallenge = c;
+  auto& info = *(currentChallenge->info);  // Dereference the pointer to get the JsonDocument
+  String game = info["game"].as<String>();
+  info["player2"] = wifi.userName;
+  challengeIsRunning = true;
+
+  if (game=="measury"){
+    State::stateFlags.set(INDEX_DISTANCE_GAME);
+  } else if(game=="voicy")
+  {
+      State::stateFlags.set(INDEX_MIC_GAME);
+  }
 
 }
--- src/functionality/challengeHandler.h
+++ src/functionality/challengeHandler.h
@@ -1,13 +1,25 @@
 #ifndef GBGAME_CHALLENGEHANDLER_H
 #define GBGAME_CHALLENGEHANDLER_H
+#include <ArduinoJson.h>
+#include "wifi/dataTransmit.h"
+
+class Challenge;
 
 
 class ChallengeHandler {
 public:
+
     static ChallengeHandler& getInstance(); // Static method to access the instance
     // Delete copy constructor and assignment operator for singleton class
     ChallengeHandler(const ChallengeHandler&) = delete;
     ChallengeHandler& operator=(const ChallengeHandler&) = delete;
+    Challenge* currentChallenge;
+
+    void startChallenge(Challenge* c);
+    bool endChallenge();
+
+    DataTransmit &wifi = DataTransmit::getInstance();
+    bool challengeIsRunning = false;
 private:
     ChallengeHandler();  // Private constructor
 };
