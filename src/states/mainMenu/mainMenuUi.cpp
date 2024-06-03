48d0230ae4e76225db312e57c066d35d96f111f5
John Forslund Gebka <johnforslundsmail@gmail.com>
2024-06-03 12:19:24 +0200

    change layout

--- src/states/mainMenu/mainMenuUi.cpp
+++ src/states/mainMenu/mainMenuUi.cpp
@@ -9,11 +9,10 @@ MainMenuUi::MainMenuUi(MainMenu *p) : c_canvas(128,128,0,0), parentState(p) {
 void MainMenuUi::init() {
 
     DataTransmit &dataTransmit = DataTransmit::getInstance();
-
-
     updateTextFields();
+    c_canvas.C.fillScreen(BLACK);
 
-    c_canvas.C.setTextColor(0xFFFF);
+    c_canvas.C.setTextColor(WHITE);
     c_canvas.C.setTextSize(2);
     c_canvas.C.setTextWrap(false);
     c_canvas.C.setCursor(18, 0);
@@ -26,34 +25,34 @@ void MainMenuUi::init() {
         c_canvas.C.drawBitmap(1, 1, image_light_bits, 16, 16, WHITE);
     }
 
-
-    c_canvas.C.drawBitmap(2, 64, image_hand_notice_bits, 16, 11, 0x555);
-
-    c_canvas.C.setTextColor(0xA800);
-    c_canvas.C.setCursor(27, 62);
-    c_canvas.C.print(textField[0]);
-
-
     c_canvas.C.setTextColor(0xFFFF);
     c_canvas.C.setTextSize(1);
-    c_canvas.C.setCursor(41, 88);
-    c_canvas.C.print(textField[1]);
-    c_canvas.C.setCursor(41, 48);
-    c_canvas.C.print(textField[4]);
-    c_canvas.C.setCursor(41, 33);
+    c_canvas.C.setCursor(21, 33);
     c_canvas.C.print(textField[3]);
-    c_canvas.C.setCursor(41, 105);
+    c_canvas.C.setCursor(21, 48);
+    c_canvas.C.print(textField[4]);
+
+    c_canvas.C.setTextSize(2);
+    c_canvas.C.setTextColor(GREEN);
+    c_canvas.C.setCursor(7, 62);
+    c_canvas.C.print(textField[0]);  //The selected state
+
+    c_canvas.C.setTextColor(WHITE);
+    c_canvas.C.setTextSize(1);
+    c_canvas.C.setCursor(21, 88);
+    c_canvas.C.print(textField[1]);
+
+    c_canvas.C.setCursor(21, 105);
+    c_canvas.C.print(textField[2]);
     c_canvas.C.print(textField[2]);
 }
 
-void MainMenuUi::drawText() {
+void MainMenuUi::drawText(){
 
     DataTransmit &dataTransmit = DataTransmit::getInstance();
      updateTextFields();
      c_canvas.C.fillScreen(BLACK);
 
-    // textField[0] = textField[4];
-
     c_canvas.C.setTextColor(WHITE);
     c_canvas.C.setTextSize(2);
     c_canvas.C.setTextWrap(false);
@@ -66,27 +65,25 @@ void MainMenuUi::drawText() {
     } else {
         c_canvas.C.drawBitmap(1, 1, image_light_bits, 16, 16, WHITE);
     }
-    c_canvas.C.drawBitmap(2, 64, image_hand_notice_bits, 16, 11, 0x555);
-
 
     c_canvas.C.setTextColor(0xFFFF);
     c_canvas.C.setTextSize(1);
-    c_canvas.C.setCursor(41, 33);
+    c_canvas.C.setCursor(21, 33);
     c_canvas.C.print(textField[3]);
-    c_canvas.C.setCursor(41, 48);
+    c_canvas.C.setCursor(21, 48);
     c_canvas.C.print(textField[4]);
 
     c_canvas.C.setTextSize(2);
-    c_canvas.C.setTextColor(RED);
-    c_canvas.C.setCursor(27, 62);
+    c_canvas.C.setTextColor(GREEN);
+    c_canvas.C.setCursor(0, 62);
     c_canvas.C.print(textField[0]);  //The selected state
 
     c_canvas.C.setTextColor(WHITE);
     c_canvas.C.setTextSize(1);
-    c_canvas.C.setCursor(41, 88);
+    c_canvas.C.setCursor(21, 88);
     c_canvas.C.print(textField[1]);
 
-    c_canvas.C.setCursor(41, 105);
+    c_canvas.C.setCursor(21, 105);
     c_canvas.C.print(textField[2]);
 }
 
